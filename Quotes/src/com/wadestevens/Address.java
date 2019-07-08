affbed70b7e48c38a90a5d7cb4554eb4863af548
twls1 <wade_stevens@unigroup.com>
2019-07-07 20:00:48 -0500

    Added to the quotes

--- Quotes/src/com/wadestevens/Address.java
+++
@@ -1,4 +0,0 @@
-package com.wadestevens;
-
-public class Address {
-}
--- Quotes/src/com/wadestevens/Orders.java
+++
@@ -1,9 +0,0 @@
-package com.wadestevens;
-
-public class Orders {
-
-    private String customerName;
-    private String originAddress;
-    private String destination;
-    
-}
